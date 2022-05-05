b18e237e04bd0b2421a21b9d77268a22e199727e
Samuel Weiss <weissbsamuel@gmail.com>
2022-05-05 15:57:06 -0400

    Added sidebar

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,3 +1,8 @@
 class Comment < ApplicationRecord
+  include Visible
+
   belongs_to :article
+
+  validates :body, presence: true, length: { minimum: 5}
+  validates :commenter, presence: true, length: {minimum: 2}
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_05_03_202611) do
+ActiveRecord::Schema[7.0].define(version: 2022_05_05_192723) do
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.text "body"
@@ -19,6 +19,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_05_03_202611) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "github"
+    t.string "status"
   end
 
   create_table "comments", force: :cascade do |t|
@@ -27,7 +28,15 @@ ActiveRecord::Schema[7.0].define(version: 2022_05_03_202611) do
     t.integer "article_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "status"
     t.index ["article_id"], name: "index_comments_on_article_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "username"
+    t.string "password_digest"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   add_foreign_key "comments", "articles"
